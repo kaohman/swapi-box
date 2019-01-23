190fc9ae9c42552c379748106d3f2262e5647d12
Karin Ohman <kaohma@gmail.com>
2019-01-22 17:12:33 -0700

    Add relevant files and setup app

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../normalize.css';
+import './App.css';
+import '../../main.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
