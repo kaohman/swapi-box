c1c82385925462303df39bc75de01666675f8081
Karin Ohman <kaohma@gmail.com>
2019-01-22 17:21:40 -0700

    Create classes and functions for components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import '../../normalize.css';
 import './App.css';
 import '../../main.scss';
+import Button from '../Button/Button';
+import CardContainer from '../CardContainer/CardContainer';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div>
+        <h1>SWAPI-BOX</h1>
+        <Button />
+        <CardContainer />
       </div>
     );
   }
---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../Card/Card';
+
+const CardContainer = (props) => {
+
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default CardContainer;
