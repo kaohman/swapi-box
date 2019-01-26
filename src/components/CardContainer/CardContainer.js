71a5ecf52893e212fe363518e7ff6d03d35bc76b
Karin Ohman <kaohma@gmail.com>
2019-01-26 11:33:05 -0700

    Add styling for loading page and favorites page

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -4,7 +4,12 @@ import PropTypes from 'prop-types';
 
 const CardContainer = ({cards, favorites, toggleFavorite, currentPage}) => {
   if (currentPage === 'favorites' && favorites.length === 0) {
-    return <h3 id='loading-text'>Any favorites right now you have not.</h3>
+    return (
+      <div className='no-cards-container'>
+        <img className='star-wars-images' src={require('../../images/yoda.png')} alt='yoda' />
+        <h3 className='no-cards-text'>Any favorites right now you have not.</h3>
+      </div>
+    )
   } else {
     return (
       <div id='card-container'>
--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Loading = () => {
   return(
-    <div>
-      <h3 id='loading-text'>Loading data... I find your lack of faith disturbing</h3>
+    <div className='no-cards-container'>
+      <img className='star-wars-images' src={require('../../images/darth-vader.png')} alt='darth vader' />
+      <h3 className='no-cards-text'>Loading data... I find your lack of faith disturbing</h3>
       <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-      <img id='darth-vader-img' src={require('../../images/darth-vader.png')} alt='darth vader' />
     </div>
   )
 }
