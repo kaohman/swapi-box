ca4acde7048309d188ffc59ec51e0ec6f083c749
Karin Ohman <kaohma@gmail.com>
2019-01-27 11:03:52 -0700

    Updates

---
+++ precache-manifest.2eb991501a1a1cda4e144ca482ce3f73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d3b9a29e944438e9e7034a2d080182b",
+    "url": "/swapi-box/static/media/yoda.6d3b9a29.png"
+  },
+  {
+    "revision": "7b06a69cf3c541b3b1d434cda05de2ed",
+    "url": "/swapi-box/static/media/stars-background2.7b06a69c.jpg"
+  },
+  {
+    "revision": "9fd74298310ce1ba5dd710cc16ed313c",
+    "url": "/swapi-box/static/media/darth-vader.9fd74298.png"
+  },
+  {
+    "revision": "37aee6c8c1d16c287d66",
+    "url": "/swapi-box/static/js/runtime~main.37aee6c8.js"
+  },
+  {
+    "revision": "6bc836abdb7c54f4a4a6",
+    "url": "/swapi-box/static/js/main.6bc836ab.chunk.js"
+  },
+  {
+    "revision": "313f6a0baf67845e41a7",
+    "url": "/swapi-box/static/js/1.313f6a0b.chunk.js"
+  },
+  {
+    "revision": "6bc836abdb7c54f4a4a6",
+    "url": "/swapi-box/static/css/main.b83e1e43.chunk.css"
+  },
+  {
+    "revision": "912cf4fd7fcdbff1289cac2b4c677723",
+    "url": "/swapi-box/index.html"
+  }
+];
